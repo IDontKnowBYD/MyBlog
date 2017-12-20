21634d4a99d1f38429ef2fc411550f3535d5ab22
zhushaofeng <zhushaofeng19960409@gmail.com>
2017-12-20 10:46:58 +0800

    update

--- webpack/webpack.build.config.js
+++ webpack/webpack.build.config.js
@@ -19,7 +19,7 @@ module.exports = merge(basicConfig, {
   },
   output: {
     path: resolve('docs'),
-    publicPath: '/Vue-Phone_Blog/',
+    publicPath: '/Vue-Phone-Blog/',
     filename: '[name].[chunkhash:8].js',
     chunkFilename: 'js/[name].[chunkhash:8].js'
   },
